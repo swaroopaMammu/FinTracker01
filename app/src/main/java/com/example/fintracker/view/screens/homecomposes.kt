c4335ff0b83deda9fc6dc0cacad0bca0143df636
swaroopaMammu <swaroopamammu1997@gmail.com>
2025-03-03 12:04:42 +0530

    removing unwanted imports

--- app/src/main/java/com/example/fintracker/view/screens/homecomposes.kt
+++ app/src/main/java/com/example/fintracker/view/screens/homecomposes.kt
@@ -1,7 +1,5 @@
 package com.example.fintracker.view.screens
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
